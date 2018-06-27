98746d2acf87950bf5bb4e82b8c2f09a5277426c
hanbj <hanbj0707@163.com>
2018-06-27 14:16:30 +0800

    cat metadata

--- core/src/test/java/org/elasticsearch/gateway/MetaStateServiceTests.java
+++ core/src/test/java/org/elasticsearch/gateway/MetaStateServiceTests.java
@@ -18,11 +18,17 @@
  */
 package org.elasticsearch.gateway;
 
+import com.carrotsearch.hppc.cursors.ObjectObjectCursor;
 import org.elasticsearch.Version;
+import org.elasticsearch.cluster.ClusterModule;
 import org.elasticsearch.cluster.metadata.IndexMetaData;
 import org.elasticsearch.cluster.metadata.MetaData;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.common.xcontent.NamedXContentRegistry;
+import org.elasticsearch.common.xcontent.ToXContent;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.env.Environment;
 import org.elasticsearch.env.NodeEnvironment;
 import org.elasticsearch.index.Index;
 import org.elasticsearch.test.ESTestCase;
@@ -101,6 +107,56 @@ public class MetaStateServiceTests extends ESTestCase {
             assertThat(loadedState.persistentSettings(), equalTo(metaData.persistentSettings()));
             assertThat(loadedState.hasIndex("test1"), equalTo(true));
             assertThat(loadedState.index("test1"), equalTo(index));
+        }
+    }
+
+    public void testCatFullState() throws Exception {
+        Settings settings = Settings.builder()
+                .put(Environment.PATH_HOME_SETTING.getKey(), "E:\\Software\\elasticsearch-5.4.2")
+                .putArray(Environment.PATH_DATA_SETTING.getKey(), new String[]{"E:\\Software\\elasticsearch-5.4.2\\data1", "E:\\Software\\elasticsearch-5.4.2\\data2"}).build();
+        final NodeEnvironment env = new NodeEnvironment(settings, new Environment(settings));
+        final NamedXContentRegistry namedXContentRegistry = new NamedXContentRegistry(ClusterModule.getNamedXWriteables());
+        final MetaStateService metaStateService = new MetaStateService(Settings.EMPTY, env, namedXContentRegistry);
+        final MetaData loadedState = metaStateService.loadFullState();
+        final XContentBuilder builder = XContentFactory.jsonBuilder().prettyPrint();
+        builder.startObject();
+        final XContentBuilder content = loadedState.toXContent(builder, ToXContent.EMPTY_PARAMS);
+        builder.endObject();
+        System.out.println(content.string());
+    }
+
+    public void testCatGlobalState() throws Exception {
+        Settings settings = Settings.builder()
+                .put(Environment.PATH_HOME_SETTING.getKey(), "E:\\Software\\elasticsearch-5.4.2")
+                .putArray(Environment.PATH_DATA_SETTING.getKey(), new String[]{"E:\\Software\\elasticsearch-5.4.2\\data1", "E:\\Software\\elasticsearch-5.4.2\\data2"}).build();
+        final NodeEnvironment env = new NodeEnvironment(settings, new Environment(settings));
+        final NamedXContentRegistry namedXContentRegistry = new NamedXContentRegistry(ClusterModule.getNamedXWriteables());
+        final MetaStateService metaStateService = new MetaStateService(Settings.EMPTY, env, namedXContentRegistry);
+        final MetaData loadedState = metaStateService.loadGlobalState();
+        final XContentBuilder builder = XContentFactory.jsonBuilder().prettyPrint();
+        builder.startObject();
+        final XContentBuilder content = loadedState.toXContent(builder, ToXContent.EMPTY_PARAMS);
+        builder.endObject();
+        System.out.println(content.string());
+    }
+
+    public void testCatIndexState() throws Exception {
+        Settings settings = Settings.builder()
+                .put(Environment.PATH_HOME_SETTING.getKey(), "E:\\Software\\elasticsearch-5.4.2")
+                .put("index.name", "hanbj")
+                .putArray(Environment.PATH_DATA_SETTING.getKey(), new String[]{"E:\\Software\\elasticsearch-5.4.2\\data1", "E:\\Software\\elasticsearch-5.4.2\\data2"}).build();
+        final NodeEnvironment env = new NodeEnvironment(settings, new Environment(settings));
+        final NamedXContentRegistry namedXContentRegistry = new NamedXContentRegistry(ClusterModule.getNamedXWriteables());
+        final MetaStateService metaStateService = new MetaStateService(Settings.EMPTY, env, namedXContentRegistry);
+        final MetaData loadedState = metaStateService.loadFullState();
+        for (ObjectObjectCursor<String, IndexMetaData> cursor : loadedState.indices()) {
+            if (cursor.key.equals(settings.get("index.name"))) {
+                final XContentBuilder builder = XContentFactory.jsonBuilder().prettyPrint();
+                builder.startObject();
+                final XContentBuilder content = cursor.value.toXContent(builder, ToXContent.EMPTY_PARAMS);
+                builder.endObject();
+                System.out.println(content.string());
+            }
         }
     }
 }
